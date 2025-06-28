232730e669f2f8d95b80ba8a76684263b6c4c3f1
cesaruacj <al234525@alumnos.uacj.mx>
2025-06-28 16:03:20 -0600

    feat: Add script to generate Flashbots key

--- scripts/generate-flashbots-key.ts
+++ scripts/generate-flashbots-key.ts
@@ -1,8 +1,8 @@
-// Crea este archivo para generar una clave privada válida
+// Script para generar una clave privada válida para Flashbots
 
-const { ethers } = require("ethers");
-const fs = require("fs");
-const path = require("path");
+import { ethers } from "ethers";
+import * as fs from "fs";
+import * as path from "path";
 
 // Generar una wallet nueva
 const wallet = ethers.Wallet.createRandom();
@@ -13,8 +13,8 @@ console.log("\nDirección asociada:");
 console.log(wallet.address);
 
 // Actualizar automáticamente el archivo .env
-const envPath = path.join(__dirname, "../.env");
-let envContent = fs.readFileSync(envPath, "utf8");
+const envPath: string = path.join(__dirname, "../.env");
+let envContent: string = fs.readFileSync(envPath, "utf8");
 
 // Reemplazar la línea existente o añadir una nueva
 if (envContent.includes("FLASHBOTS_KEY=")) {
